fd924cafbde0970484e2f372c9ced67ada2281f9
Danish Singh Sethi <34684132+DSS3113@users.noreply.github.com>
2022-11-25 04:19:52 -0600

    Highlight according to classification

--- ner_chrome_extension/NER/backend.py
+++ ner_chrome_extension/NER/backend.py
@@ -14,7 +14,12 @@ api = Api(app)
 tokenizer = AutoTokenizer.from_pretrained("dslim/bert-large-NER")
 model = AutoModelForTokenClassification.from_pretrained("dslim/bert-large-NER")
 ner = pipeline("ner", model=model, tokenizer=tokenizer)
-
+colors = {
+    'PER': 'red',
+    'ORG': 'orange',
+    'LOC': 'yellow',
+    'MISC': 'green'
+}
 
 def get_entities(data):
     entities = set()
@@ -22,23 +27,33 @@ def get_entities(data):
         ner_results = ner(sent)
         for result in ner_results:
             word = str(result['word'])
+            entity_type = str(result['entity'][result['entity'].find('-')+1:])
             if len(word) < 4 or '#' in word:
                 continue
-            entities.add(word)
+            entities.add((word, entity_type))
     return tuple(entities)
-        
+
+
+"""
+PER: red
+ORG: orange
+LOC: yellow
+MISC: green
+"""
 class NamedEntities(Resource):
     def post(self):
         data = request.get_json()
         entities = get_entities(data['body_text'])
         body_html = data['body_html']
         for entity in entities:
-            body_html = body_html.replace(f" {entity} ", f" <span style='background-color: yellow'>{entity}</span> ")\
-                .replace(f" {entity}.", f" <span style='background-color: yellow'>{entity}</span>.")\
-                .replace(f" {entity},", f" <span style='background-color: yellow'>{entity}</span>,")\
-                .replace(f" {entity}:", f" <span style='background-color: yellow'>{entity}</span>:")\
-                .replace(f" {entity}–", f" <span style='background-color: yellow'>{entity}</span>–")\
-                .replace(f" {entity}\n", f" <span style='background-color: yellow'>{entity}</span>\n")
+            word = entity[0]
+            color = colors[entity[1]]
+            body_html = body_html.replace(f" {entity[0]} ", f" <span style='background-color: {color}'>{word}</span> ")\
+                .replace(f" {word}.", f" <span style='background-color: {color}'>{word}</span>.")\
+                .replace(f" {word},", f" <span style='background-color: {color}'>{word}</span>,")\
+                .replace(f" {word}:", f" <span style='background-color: {color}'>{word}</span>:")\
+                .replace(f" {word}–", f" <span style='background-color: {color}'>{word}</span>–")\
+                .replace(f" {word}\n", f" <span style='background-color: {color}'>{word}</span>\n")
         return {'body_html': body_html}
 
 api.add_resource(NamedEntities, "/")
